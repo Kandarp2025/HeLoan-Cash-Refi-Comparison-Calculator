c0f59f1e24c377650175f3de0791a2b69855abbf
Kandarp <kandarp099@gmail.com>
2026-04-16 18:08:14 +0530

    remove dist n node_modules

--- dist/routes/healthRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const healthController_1 = require("../controllers/healthController");
-const healthRoutes = (0, express_1.Router)();
-healthRoutes.get("/health", healthController_1.healthController);
-exports.default = healthRoutes;
-//# sourceMappingURL=healthRoutes.js.map
--- dist/services/unitedFinancialCalculator.service.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.runUnitedFinancialCalculation = void 0;
-const unitedFinancialCalculator_1 = require("../shared/unitedFinancialCalculator");
-const runUnitedFinancialCalculation = (input) => {
-    return (0, unitedFinancialCalculator_1.calculateComparison)(input);
-};
-exports.runUnitedFinancialCalculation = runUnitedFinancialCalculation;
-//# sourceMappingURL=unitedFinancialCalculator.service.js.map
